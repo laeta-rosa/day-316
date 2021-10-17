6fb157454da88a6fb97bdc1c88695c8550c3912b
Hudayberdiyeva <maysa.hudayberdiyeva@accenture.com>
2021-10-17 16:02:19 +0300

    config

--- src/main/java/app/BotApplication.java
+++ src/main/java/app/BotApplication.java
@@ -1,7 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@SpringBootApplication
 public class BotApplication {
 
     public static void main(String[] args) {
